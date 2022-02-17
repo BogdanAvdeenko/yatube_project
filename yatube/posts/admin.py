4379fdf1523b5fe5364943df3143ab624d2c930e
Bogdan Avdeenko <bogdan.avdeenko@gmail.com>
2022-02-17 20:18:55 +0300

    Регистрация модели Post в админке

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+# Из модуля models импортируем модель Post
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Post, PostAdmin)
