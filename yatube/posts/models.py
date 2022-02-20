0d31663dc13e3bc7c9612a09cc05d5eaccdca895
Bogdan Avdeenko <bogdan.avdeenko@gmail.com>
2022-02-20 16:27:09 +0300

    Всё готово?

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 from django.contrib.auth import get_user_model
 
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
 User = get_user_model()
 
 
@@ -12,4 +22,11 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='posts'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='posts_group',
     )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
-    template = 'posts/index.html'
-    title = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': title
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    title = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title
+        'group_list': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
